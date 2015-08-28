a05b679a8dc33d59d61db0143b6f595b5a581c15
Nicolas Domarin <nicolas.domarin@gmail.com>
2015-08-28 12:13:30 +0200

    somme addings

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ActiveRecord::Base
   belongs_to :sale
   has_many :order_items
   
-  has_attached_file :image,:default_url => "/images/:style/missing.png"# , :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  #validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :image,:default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
