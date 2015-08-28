5326bbbdf8e4611ed9e23a7037f96ca6f4f47a3d
Nicolas Domarin <nicolas.domarin@gmail.com>
2015-08-28 11:49:55 +0200

    Fix Image Issue

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,5 @@ class Product < ActiveRecord::Base
   has_many :order_items
   
   has_attached_file :image,:default_url => "/images/:style/missing.png"# , :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  #validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
